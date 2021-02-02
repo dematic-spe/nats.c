5d00100a1050f9f87c210d1942fb3d1ca6ab3013
Matt Weiss <matthew.weiss@dematic.com>
2021-02-02 13:27:01 -0500

    updates.

--- build-nats.bat
+++ build-nats.bat
@@ -1,20 +1,34 @@
-@echo.
-@echo This script will build nats.c for windows. it assumes you are running in the
-@echo "x86_x64 Cross Tools Command Prompt for VS 2019" command shell. if you are not
-@echo stop this script and first run this command shell
 
-PAUSE
 
-mkdir repos
+@echo off
 
-REM pull vcpckg and build the 2020.11 tag 
-git clone https://github.com/microsoft/vcpkg.git repos\vcpkg
-cd repos\vcpkg 
-git checkout 2020.11
-cmd /c bootstrap-vcpkg.bat
-vcpkg install zlib openssl Protobuf --triplet x64-windows
+set ERROR_CODE=0
 
-cd %~dp0
+@REM ==== START VALIDATION ====
+if not "%VCPKG_HOME%"=="" goto OK_VCPKG_HOME
+echo The VCPKG_HOME environment variable is not defined correctly >&2
+echo This environment variable is needed to run this program >&2
+goto error
+
+echo The VCPKG_HOME environment variable exists
+:OK_VCPKG_HOME
+@REM Check if vcpkg is checked out, if not then check it out
+if exist "%VCPKG_HOME%\bootstrap-vcpkg.bat" goto VCPKG_GIT
+echo The VCPKG_HOME environment variable exists but the code is not checked out, so it will be checked out >&2
+git clone https://github.com/microsoft/vcpkg.git %VCPKG_HOME%
+
+:VCPKG_GIT
+REM echo The VCPKG_HOME git project is checked out
+set VCPKG_CMD="%VCPKG_HOME%/vcpkg.exe"
+if exist "%VCPKG_CMD%" goto VCPKG_BUILT
+call "%VCPKG_HOME%\bootstrap-vcpkg.bat"
+
+:VCPKG_BUILT
+@REM ==== Checking if libraries are built ====
+REM echo The %VCPKG_CMD% has already been built
+if not exist "%VCPKG_HOME%/installed/x64-windows/include/openssl"   %VCPKG_CMD% install openssl --triplet x64-windows
+if not exist "%VCPKG_HOME%/installed/x64-windows/include/zlib.h"      %VCPKG_CMD% install zlib --triplet x64-windows
+if not exist "%VCPKG_HOME%/installed/x64-windows/include/Protobuf"     %VCPKG_CMD% install Protobuf --triplet x64-windows
 
 REM pull protobuf-c and switch to a known tag 
 git clone https://github.com/protobuf-c/protobuf-c.git repos\protobuf-c
@@ -33,7 +47,14 @@ copy ..\repos\protobuf-c\protobuf-c\protobuf-c.c ..\src\protobuf-c.c
 mkdir  ..\src\protobuf-c\
 copy ..\repos\protobuf-c\protobuf-c\protobuf-c.h ..\src\protobuf-c\protobuf-c.h
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE=..\repos\vcpkg\scripts\buildsystems\vcpkg.cmake -DNATS_BUILD_STREAMING=ON
+cmake .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_HOME%\scripts\buildsystems\vcpkg.cmake -DNATS_BUILD_STREAMING=ON
 
 cmake --build . --config Debug
 cmake --build . --config Release
+
+goto END
+
+:error
+set ERROR_CODE=1
+
+:END
